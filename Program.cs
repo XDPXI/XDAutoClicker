c10103d0d37f33a3d4ecbeffc7dbd77fc5279741
XDPXI <xdpxigaming@gmail.com>
2025-05-15 19:42:00 +1000

    Reformatted Code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace XDAutoClicker
     internal static class Program
     {
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
